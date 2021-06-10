2a25540112320891e672e4023413396a90c7e9a8
jensnti <jensandreasson77@gmail.com>
2021-06-10 15:31:40 +0200

    ♻️🐛color .length

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,13 +1,24 @@
 const colors = require('nice-color-palettes');
 
-function getRandomIntInclusive(min, max) {
+const getRandomIntInclusive = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
-}
+};
 
-const color = colors[getRandomIntInclusive(0, colors.length)];
-const pick = color[getRandomIntInclusive(0, color.length)];
+// const color = colors[getRandomIntInclusive(0, colors.length)];
+// const pick = color[getRandomIntInclusive(0, color.length)];
+
+const getColorPick = () => {
+    try {
+        const color = colors[getRandomIntInclusive(0, colors.length)];
+        const pick = color[getRandomIntInclusive(0, color.length)];
+        return pick;
+    } catch (e) {
+        console.log(e);
+        return '#121212';
+    }
+};
 
 module.exports = {
     // NOTE: `process.env.URL` is provided by Netlify, and may need
@@ -15,7 +26,7 @@ module.exports = {
     url: process.env.URL || 'http://localhost:8080',
     language: 'sv',
     siteName: 'Webbutveckling 1',
-    themeColor: pick, // replace with themeColor: pick, for a random color '#f3722c'
+    themeColor: getColorPick(), // replace with themeColor: pick, for a random color '#f3722c'
     siteDescription:
         'Kurssida för webbutveckling. HTML, CSS, javascript, tillgänglighet',
     author: {
