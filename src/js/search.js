203cba1fb53ddbed0747e984d833d3d1522533f6
jensnti <jensandreasson77@gmail.com>
2021-06-11 19:42:46 +0200

    ✨🚧search function

---
+++ src/js/search.js
@@ -0,0 +1,49 @@
+window.addEventListener('load', () => {
+    const search = (e) => {
+        const results = window.searchIndex.search(e.target.value, {
+            bool: 'OR',
+            expand: true
+        });
+
+        const resultElement = document.querySelector('#search-results');
+
+        resultElement.innerHTML = '';
+        if (results && results.length > 0) {
+            resultElement.classList.add('show');
+            results.map((result) => {
+                const { id, title, excerpt } = result.doc;
+                const li = document.createElement('li');
+                li.classList.add('dropdown-item');
+                resultElement.appendChild(li);
+
+                const a = document.createElement('a');
+                a.setAttribute('href', id);
+                a.classList.add('stretched-link');
+                a.textContent = title;
+                li.appendChild(a);
+
+                const p = document.createElement('p');
+                p.textContent = excerpt;
+                li.appendChild(p);
+
+                window.addEventListener('click', function handler() {
+                    resultElement.classList.remove('show');
+                    resultElement.innerHTML = '';
+                    document.querySelector('#search-input').value = '';
+                    window.removeEventListener('click', handler);
+                });
+            });
+        } else {
+            resultElement.classList.remove('show');
+        }
+    };
+
+    fetch('/search-index.json').then((response) =>
+        response.json().then((rawIndex) => {
+            window.searchIndex = elasticlunr.Index.load(rawIndex);
+            document
+                .querySelector('#search-input')
+                .addEventListener('input', search);
+        })
+    );
+});
